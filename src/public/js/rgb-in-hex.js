648746bdd775020d728cdf6da93607078213be6a
Dmitry <dimaruzaev2@gmai.com>
2022-05-21 22:18:57 +0300

    v0.1.2 - add doc in rgb-in-hex.js

--- src/public/js/rgb-in-hex.js
+++ src/public/js/rgb-in-hex.js
@@ -2,7 +2,12 @@
 
 let endNum = '';
 
-// * вывод каналов rgb
+/**
+ * вывод каналов rgb
+ *
+ * @param {number} countName
+ * @returns
+ */
 const writeChannelRGB = (countName) => {
 	let nameChannel = '';
 
@@ -23,28 +28,36 @@ const writeChannelRGB = (countName) => {
 };
 
 
-// * конвертация в 16.ричную с.с.
+/**
+ * конвертация в 16 с/c
+ *
+ * @param {number} intNum - rgb калан из фун-ции writeChannelRGB
+ */
 const convertChannel = (intNum) => {
-	let restIntNum;
-	let numSixTen = '';
+	let restIntNum; // остаток от деления intNum на 16
+	let numSixTen = ''; // канал в 16 с/с
 
 	while (intNum >= 16) {
 		restIntNum = intNum % 16;
-		intNum = Math.trunc(intNum / 16);
+		intNum = Math.trunc(intNum / 16); // целочисленный остаток
 
 		restIntNum > 9 ? numSixTen += convertMaxNum(restIntNum) : numSixTen += restIntNum;
 	}
 
-	intNum > 9 ? numSixTen += convertMaxNum(intNum) : numSixTen += intNum;
+	intNum > 9 ? numSixTen += convertMaxNum(intNum) : numSixTen += intNum; // прибавляем целочисленный остаток к обычным остаткам
 
 	if (numSixTen.length === 2)
 		endNum += numSixTen.split('').reverse().join('');
 	else
-		endNum += '0' + numSixTen;
+		endNum += '0' + numSixTen; // если в канале 1 цифра, то добавляем 0 в самом начале
 };
 
 
-// * конвертация 10 & > значения в 16.ричный формат
+/**
+ * конвертация 10 & > значения в 16 с/с
+ * @param {number} num - канал в 16 с/с
+ * @returns {string} - преобразованный канал в 16 с/с
+ */
 const convertMaxNum = (num) => {
 	switch (num) {
 		case 10:
