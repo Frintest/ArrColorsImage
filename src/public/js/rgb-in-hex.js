e34d7613c1f27bdc54ff19535128b535d9f24b23
Dmitry <dimaruzaev2@gmai.com>
2022-05-22 17:21:57 +0300

    v1.0.3 - add resistance to gigo

--- src/public/js/rgb-in-hex.js
+++ src/public/js/rgb-in-hex.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let endNum = '';
+let arrChannels = [];
 
 /**
  * вывод каналов rgb
@@ -22,34 +23,9 @@ const writeChannelRGB = (countName) => {
 			nameChannel = 'B';
 	}
 
-	let channel = Number(prompt(`Введите ${nameChannel}`));
+	arrChannels[countName] = prompt(`Введите ${nameChannel}`);
 
-	return channel;
-};
-
-
-/**
- * конвертация в 16 с/c
- *
- * @param {number} intNum - rgb калан из фун-ции writeChannelRGB
- */
-const convertChannel = (intNum) => {
-	let restIntNum; // остаток от деления intNum на 16
-	let numSixTen = ''; // канал в 16 с/с
-
-	while (intNum >= 16) {
-		restIntNum = intNum % 16;
-		intNum = Math.trunc(intNum / 16); // целочисленный остаток
-
-		restIntNum > 9 ? numSixTen += convertMaxNum(restIntNum) : numSixTen += restIntNum;
-	}
-
-	intNum > 9 ? numSixTen += convertMaxNum(intNum) : numSixTen += intNum; // прибавляем целочисленный остаток к обычным остаткам
-
-	if (numSixTen.length === 2)
-		endNum += numSixTen.split('').reverse().join('');
-	else
-		endNum += '0' + numSixTen; // если в канале 1 цифра, то добавляем 0 в самом начале
+	return arrChannels[countName];
 };
 
 
@@ -83,8 +59,40 @@ const convertMaxNum = (num) => {
 };
 
 
+/**
+ * конвертация в 16 с/c
+ *
+ * @param {number} intNum - rgb калан из фун-ции writeChannelRGB
+ */
+const convertChannel = (intNum) => {
+	let restIntNum; // остаток от деления intNum на 16
+	let numSixTen = ''; // канал в 16 с/с
+
+	while (intNum >= 16) {
+		restIntNum = intNum % 16;
+		intNum = Math.trunc(intNum / 16); // целочисленный остаток
+
+		restIntNum > 9 ? numSixTen += convertMaxNum(restIntNum) : numSixTen += restIntNum;
+	}
+
+	intNum > 9 ? numSixTen += convertMaxNum(intNum) : numSixTen += intNum; // прибавляем целочисленный остаток к обычным остаткам
+
+	if (numSixTen.length === 2)
+		endNum += numSixTen.split('').reverse().join(''); /* array in string */
+	else
+		endNum += '0' + numSixTen; // если в канале 1 цифра, то добавляем 0 в самом начале
+};
+
+
 for (let i = 0; i < 3; i++) {
-	convertChannel(writeChannelRGB(i));
+	writeChannelRGB(i);
+
+	if (!isNaN(Number(arrChannels[i])) && arrChannels[i].trim() != '') convertChannel(Number(arrChannels[i])); /* ! isNaN(Number(variables));*/
+	else {
+		console.error('Channel not a number, programm stoping');
+		endNum = NaN;
+		break;
+	}
 }
 
 console.log(endNum);
